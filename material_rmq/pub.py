c7ee6d984620030eaa33dcd97a754bfce1951e8e
Nalana <habibiysuf@gmail.com>
2021-08-09 09:04:09 +0700

    ftp & rmq

---
+++ material_rmq/pub.py
@@ -0,0 +1,14 @@
+import pika
+
+credentials = pika.PlainCredentials('tmdgdai', 'tmdgdai')
+parameters = pika.ConnectionParameters('rmq1.pptik.id',5672, '/',credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+# channel.exchange_declare(exchange='logs', exchange_type='fanout')
+# result = channel.queue_declare(queue='', exclusive=True)
+# queue_name = result.method.queue
+# channel.queue_bind(exchange='logs', queue=queue_name)
+
+channel.basic_publish(exchange='', routing_key='pkl_juli_2021_1', body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
---
+++ material_rmq/sub.py
@@ -0,0 +1,25 @@
+import pika, sys, os
+
+def main():
+    credentials = pika.PlainCredentials('tmdgdai', 'tmdgdai')
+    parameters = pika.ConnectionParameters('rmq1.pptik.id',5672, '/',credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue='pkl_juli_2021_1')
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    channel.basic_consume(queue='pkl_juli_2021_1', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
